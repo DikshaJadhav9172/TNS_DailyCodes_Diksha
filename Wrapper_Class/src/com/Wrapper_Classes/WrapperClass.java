7c24474bc0b9e86907b3b990a007f5b16c9692dd
jadha <jadhavd20201@gmail.com>
2025-07-23 10:58:26 +0530

    Wrapper Classes

---
+++ Wrapper_Class/src/com/Wrapper_Classes/WrapperClass.java
@@ -0,0 +1,28 @@
+package com.Wrapper_Classes;
+
+public class WrapperClass {
+	
+	public static void main(String[] args) {
+		//Unboxing
+		Integer i=new Integer(10);
+		System.out.println(i);
+		
+		int b=i.intValue();
+		System.out.println(b);
+		
+		//Autoboxing
+		int a=100;
+		Integer i1=a;
+		System.out.println(i1);
+		
+		String s="11";
+		String s1="10";
+		System.out.println(s+s1);
+		int x=Integer.parseInt(s);
+		int y=Integer.parseInt(s1);
+		int add=x+y;
+		System.out.println(add);
+		
+		
+	}
+}
